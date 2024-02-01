49013e553a5f914af85686d6187b0891b0b3d18f
Prgupta25 <120772967+Prgupta25@users.noreply.github.com>
2024-02-01 17:29:18 +0530

    Add files via upload

---
+++ bit-masking-(get)-(and operator).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void get_bit(int n, int k){
+    if(n&(1<<(k-1))){
+        cout<<true<<endl;
+    }
+    else{
+        cout<<false<<endl;
+    }
+}
+int main(){
+    int n,k;
+    cin>>n>>k;
+    get_bit(n,k);
+    return 0;
+
+}

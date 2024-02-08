5f07c226efd2f29733d2daa8682e7a507924558e
Prgupta25 <120772967+Prgupta25@users.noreply.github.com>
2024-02-08 17:30:19 +0530

    Add files via upload

---
+++ unique-path.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> f(m, vector<int>(n));
+        f[0][0] = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i) {
+                    f[i][j] += f[i - 1][j];
+                }
+                if (j) {
+                    f[i][j] += f[i][j - 1];
+                }
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+};
+
+int main() {
+    Solution solution;
+    int m = 3;
+    int n = 7;
+    cout << "Number of unique paths in a " << m << "x" << n << " grid: ";
+    cout << solution.uniquePaths(m, n) << endl;
+    return 0;
+}

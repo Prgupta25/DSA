f3b3d61763c691630c1c1d1e3dad018da1454041
Prgupta25 <120772967+Prgupta25@users.noreply.github.com>
2024-02-28 17:53:11 +0530

    Add files via upload

--- stack.cpp
+++ stack.cpp
@@ -1,24 +1,60 @@
-// jo sabse last mei element jayega vo sabse phele nikalega
-// last iin first out
-#include<iostream>
-#include<stack>
+#include<bits/stdc++.h>
 using namespace std;
+class Stack{
+public:
+    int* arr;
+    int capacity;
+    int Top;
+    Stack(int cap){
+        capacity=cap;
+        arr=new int[cap];
+        Top==-1;
+    }
+    void push(int x){
+        if(Top==capacity-1){
+            cout<<"Stack overflow"<<endl;
+            return;
+        }
+        Top++;
+        arr[Top]=x;
+    }
+    void pop(){
+        if(Top==-1){
+            cout<<"Under overflow"<<endl;
+            return;
+        }
+        Top--;
+    }
+    int peak(){
+        if(Top==-1){
+            cout<<"There is no element in the stack"<<endl;
+            return -1;
+        }
+        return arr[Top];
+    }
+    bool isEmpty(){
+        return Top==-1;
+    }
+    int size(){
+        return Top + 1;
+    }
+};
+void display(Stack s){
+    while(!s.isEmpty()){
+        cout<<s.peak()<<" ";
+        s.pop();
+    }
+}
 int main(){
-
-    stack<string> s;
-    s.push("pranav");
-    s.push("gupta");
-    s.push("kumar");
-
-    cout<<"top element "<<s.top()<<endl;
-
+    Stack s(4);
     s.pop();
-    cout<<"top element "<<s.top()<<endl;
-    
-    cout<<"size of the stack"<<s.size()<<endl;
-
-    cout<<"empty or not"<<s.empty()<<endl;
-
-
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.pop();
+    s.push(5);
+    display(s);
     return 0;
 }
